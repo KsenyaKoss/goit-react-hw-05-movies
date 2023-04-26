5993ffc0fc3beaa6df0b85ecf0ef6e0192913691
KsenyaKoss <roxenn34@gmail.com>
2023-04-26 12:50:48 +0200

    basename1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ img{
 `;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/KsenyaKoss/goit-react-hw-05-movies'>
+  <BrowserRouter basename='/goit-react-hw-05-movies'>
     <React.StrictMode>
       <App />
       <Global />
